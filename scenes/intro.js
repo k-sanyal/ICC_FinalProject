2ec2037013d43dfd24f7d014266baa65f242f25b
k-sanyal <72350751+k-sanyal@users.noreply.github.com>
2025-12-03 07:12:54 +0900

    Create intro.js

---
+++ scenes/intro.js
@@ -0,0 +1,40 @@
+class IntroScene {
+  constructor(main) {
+    this.main = main;
+    this.buttonHover = false;
+  }
+
+  start() {}
+
+  draw() {
+    background(20);
+    fill(255);
+    textSize(38);
+    textAlign(CENTER);
+    text("Hidden Meanings: An Interactive Exploration", width/2, height/2 - 80);
+
+    textSize(18);
+    text("An experiment in dual imagery, metaphor, and participation.", width/2, height/2 - 20);
+
+    let bx = width/2 - 100;
+    let by = height/2 + 40;
+    let bw = 200;
+    let bh = 60;
+
+    this.buttonHover =
+      mouseX > bx && mouseX < bx + bw && mouseY > by && mouseY < by + bh;
+
+    fill(this.buttonHover ? 220 : 180);
+    rect(bx, by, bw, bh, 10);
+
+    fill(20);
+    textSize(22);
+    text("Begin", width/2, by + 38);
+  }
+
+  mousePressed() {
+    if (this.buttonHover) {
+      this.main.showScene("scene1");
+    }
+  }
+}
