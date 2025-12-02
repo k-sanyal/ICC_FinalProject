091764997dea4de9dd062e4b7c4f421fca1a91e8
k-sanyal <72350751+k-sanyal@users.noreply.github.com>
2025-12-03 07:16:33 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,6 +12,7 @@ function setup() {
   main.addScene("sketch1", new Scene1(main));
   main.addScene("sketch2", new Scene2(main));
   main.addScene("sketch3", new Scene3(main));
+  main.addScene("sketch4", new Scene4(main));
 
   main.showScene("intro");
 }
