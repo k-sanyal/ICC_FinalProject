2c75eb7c14b9c3884e0a69be477577bbaaead66b
k-sanyal <72350751+k-sanyal@users.noreply.github.com>
2025-12-06 15:44:53 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,10 +9,10 @@ function setup() {
   main = new SceneMain();
 
   main.addScene("intro", new IntroScene(main));
-  main.addScene("sketch1", new Scene1(main));
-  main.addScene("sketch2", new Scene2(main));
-  main.addScene("sketch3", new Scene3(main));
-  main.addScene("sketch4", new Scene4(main));
+  //main.addScene("sketch1", new Scene1(main));
+  //main.addScene("sketch2", new Scene2(main));
+  //main.addScene("sketch3", new Scene3(main));
+  //main.addScene("sketch4", new Scene4(main));
 
   main.showScene("intro");
 }
